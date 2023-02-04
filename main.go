6a02063859fbed3883d1ba4e5f39a164d2caa648
kryr34 <uhamia@protonmail.com>
2023-02-04 21:22:44 +0800

    add online message and fix type

--- main.go
+++ main.go
@@ -31,8 +31,11 @@ func main() {
 	for range time.Tick(time.Minute * 15) {
 		srvInfo, _ := getStatus(mcsrvAddress)
 		if srvInfo.Status == Offline && lastStat == Online {
-			webhook.sentMessage("OOF! ther server apparently broken")
+			webhook.sentMessage("OOF! the server apparently broken")
 		} else {
+			if srvInfo.Status == Online && lastStat == Offline {
+				webhook.sentMessage("POG! the server is working again")
+			}
 			if srvInfo.CurrecntPlayer == lastAP {
 				//pass
 			} else if srvInfo.CurrecntPlayer > lastAP {
